b5cc1fab9095468a0e4527e64a7bee9349f24a1f
dabodero <44667850+dabodero@users.noreply.github.com>
2018-11-28 21:56:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,6 +4,10 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const schedule = require('node-schedule')
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 
 /**
