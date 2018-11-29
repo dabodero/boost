d65e86fd5db7d33fc9c300280cfd58f2571519c0
dabodero <44667850+dabodero@users.noreply.github.com>
2018-11-29 08:28:57 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -39,7 +39,7 @@ bot.on('ready', () => {
  * - *  -> Tous les mois
  * - *  -> Tous les jours de la semaine (Mais on s'en fout puisque tu dis que c'est le premier jour du mois)
 */
-schedule.scheduleJob('1 34 22 28 11 3', function(){
+schedule.scheduleJob('1 30 8 29 11 4', function(){
 
 
   bot.channels.get("480747263090753537").send(
