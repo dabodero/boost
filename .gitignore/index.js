f1b67f581d997daa8ed6da4178f10b4c65fa4fc5
dabodero <44667850+dabodero@users.noreply.github.com>
2018-11-29 09:01:43 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,10 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const schedule = require('node-schedule')
-
+var http = require("http");
+setInterval(function() {
+    http.get("zeubiboost.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 /**
  * Quand le bot est prêt
  */
