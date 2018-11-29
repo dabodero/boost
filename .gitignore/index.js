779d71c4fbc99e64fd984512dad1d02662d2e2a9
dabodero <44667850+dabodero@users.noreply.github.com>
2018-11-29 08:41:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const schedule = require('node-schedule')
-var http = require("http");
 
 /**
  * Quand le bot est prêt
@@ -35,7 +34,7 @@ bot.on('ready', () => {
  * - *  -> Tous les mois
  * - *  -> Tous les jours de la semaine (Mais on s'en fout puisque tu dis que c'est le premier jour du mois)
 */
-schedule.scheduleJob('1 38 8 29 11 4', function(){
+schedule.scheduleJob('1 40 8 29 11 4', function(){
 
 
   bot.channels.get("480747263090753537").send(
